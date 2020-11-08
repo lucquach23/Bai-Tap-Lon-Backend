7c3d4f12b676a0ecade493058233b3c7a7f7239c
lucquach23 <hoangluc1002@gmail.com>
2020-11-08 10:58:50 +0700

    up

---
+++ API/Controllers/SubjectsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubjectsController : ControllerBase
+    {
+        private readonly RegisterSubjectDBContext _context;
+
+        public SubjectsController(RegisterSubjectDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Subjects
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Subject>>> GetSubjects()
+        {
+            return await _context.Subjects.ToListAsync();
+        }
+
+        // GET: api/Subjects/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Subject>> GetSubject(string id)
+        {
+            var subject = await _context.Subjects.FindAsync(id);
+
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            return subject;
+        }
+
+        // PUT: api/Subjects/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSubject(string id, Subject subject)
+        {
+            if (id != subject.IdSubject)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(subject).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Subjects
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Subject>> PostSubject(Subject subject)
+        {
+            _context.Subjects.Add(subject);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SubjectExists(subject.IdSubject))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetSubject", new { id = subject.IdSubject }, subject);
+        }
+
+        // DELETE: api/Subjects/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Subject>> DeleteSubject(string id)
+        {
+            var subject = await _context.Subjects.FindAsync(id);
+            if (subject == null)
+            {
+                return NotFound();
+            }
+
+            _context.Subjects.Remove(subject);
+            await _context.SaveChangesAsync();
+
+            return subject;
+        }
+
+        private bool SubjectExists(string id)
+        {
+            return _context.Subjects.Any(e => e.IdSubject == id);
+        }
+    }
+}
